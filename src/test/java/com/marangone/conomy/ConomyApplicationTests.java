29820fa7f4e25005b727fefc9bb5786e63db2a92
mmarangone <matiasgabrielmarangone@gmail.com>
2023-10-26 19:36:30 -0300

    Inicial

---
+++ src/test/java/com/marangone/conomy/ConomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marangone.conomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
